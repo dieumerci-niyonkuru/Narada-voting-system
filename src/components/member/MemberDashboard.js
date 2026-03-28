46c48fc7a3dac7c193e18b07a5580e4733d7e8d3
Dieu Merci <dieumercin21@gmail.com>
2026-03-28 17:10:09 +0200

    fix: add missing MemberReports import

--- src/components/member/MemberDashboard.js
+++ src/components/member/MemberDashboard.js
@@ -1,3 +1,4 @@
+import MemberReports from './MemberReports'
 import React, { useState, useEffect } from 'react'
 import { supabase } from '../../utils/supabase'
 import { useNavigate } from 'react-router-dom'
